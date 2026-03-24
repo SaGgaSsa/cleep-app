a777eab5b3a837fc95edcb1a603566520486fbf4
SaGgaSsa <saggas.apoc@gmail.com>
2026-03-24 03:22:54 +0000

    feat: integrate space grotesk font

--- app/src/main/kotlin/dev/cleep/app/core/designsystem/theme/Type.kt
+++ app/src/main/kotlin/dev/cleep/app/core/designsystem/theme/Type.kt
@@ -1,12 +1,20 @@
 package dev.cleep.app.core.designsystem.theme
 
+import androidx.compose.ui.text.font.Font
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import dev.cleep.app.R
 
-private val CleepFontFamily = FontFamily.SansSerif
+private val CleepFontFamily = FontFamily(
+    Font(R.font.space_grotesk_light, FontWeight.Light),
+    Font(R.font.space_grotesk_regular, FontWeight.Normal),
+    Font(R.font.space_grotesk_medium, FontWeight.Medium),
+    Font(R.font.space_grotesk_semibold, FontWeight.SemiBold),
+    Font(R.font.space_grotesk_bold, FontWeight.Bold),
+)
 
 val CleepTypography = Typography(
     displayLarge = TextStyle(
